7d0389b0e814a14b4bf24f3252125f4f37bb1c07
Abdullah <abd00s@users.noreply.github.com>
2014-10-21 13:46:58 -0400

    completed the exercise

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 name = gets.chomp.strip
 puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.chomp.strip.to_i
+puts "You were born in the Year #{Time.now.year - age}" #Program assumes birth month passed already
+
